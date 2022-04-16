0db90798dc0132b9b5d9e02501b89ce7be47e364
kawabe <kawa-kawa-bebebe@i.softbank.jp>
2022-04-16 15:31:48 +0900

    execel受け取り完了 条件分けの実装方法にとりかかる

--- src/main/java/jp/righton/analize/HomeController.java
+++ src/main/java/jp/righton/analize/HomeController.java
@@ -1,13 +1,13 @@
 package jp.righton.analize;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.swing.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -18,18 +18,18 @@ import java.util.UUID;
 //プロJavaではHomeコントローラーの（”/hello")
 @Controller
 public class HomeController {
-    record TaskItem(String id, String numRankCompany, String numRankBlock, String numRankStore, String Group,
-                    String itemNumber, String itemName, double numSalesPoint){}
+    record TaskItem(String id, Cell rankCompany, Cell rankBlock, Cell rankStore, Cell Group,
+                    Cell itemNumber, Cell itemName, Cell salesPoint){}
     private List<TaskItem> taskItems = new ArrayList<>();
 
     @RequestMapping("/list")
     String listItems(Model model) {
-                model.addAttribute("taskLists", taskItems);
+                model.addAttribute("taskList", taskItems);
                 return "home";
     }
 
-    @GetMapping("/add")
-    public static String bestSeller(@RequestParam("UpLoadFile")String UpLoadFile)throws EncryptedDocumentException, IOException {
+    @PostMapping("/add")
+    public String bestSeller(@RequestParam("UpLoadFile") MultipartFile UpLoadFile)throws EncryptedDocumentException, IOException {
         //エクセルファイルへアクセス
         Workbook excel = WorkbookFactory.create(new File("C://tenpo_hinban_jisseki_1319_202230.xlsx"));
         // エクセルシート名
@@ -54,34 +54,20 @@ public class HomeController {
             //24番目のセル（当週売点）
             Cell salesPoint = row.getCell(23);
 
+            String id = UUID.randomUUID().toString().substring(0, 8);
+            TaskItem item = new TaskItem(id, rankCompany, rankBlock, rankStore,
+                    Group, itemNumber, itemName, salesPoint);
+            taskItems.add(item);
 
-            //全社順位、ブロック順位、自店順位,当週売れ点を実数（.いくつ表記）に変換
 
-            double numRankCompany = Double.parseDouble(String.valueOf(rankCompany));
-            double numRankBlock = Double.parseDouble(String.valueOf(rankBlock));
-            double numRankStore = Double.parseDouble(String.valueOf(rankStore));
-            double numSalesPoint = Double.parseDouble(String.valueOf(salesPoint));
 
-            if (numSalesPoint >= 5.0 && numRankCompany <= 3 && numRankStore <= 3.0) {
-                //取得した文字列の表示
-                System.out.println("売れ筋アイテム");
-                System.out.println("全社順位 : " + numRankCompany);
-                System.out.println("ブロック順位 : " + numRankBlock);
-                System.out.println("自店順位 : " + numRankStore);
-                System.out.println("部門 : " + Group);
-                System.out.println("品番 : " + itemNumber);
-                System.out.println("品名 : " + itemName);
-                System.out.println("当週売点 : " + numSalesPoint);
-                System.out.println("----------------------");
             }
-        }
-
-        return UpLoadFile;
+        return "redirect:/list";
     }
 
 
-    String addItem( /*@RequestParam("UpLoadFile") MultipartFile multipartFile , */
-                   @RequestParam("numRankCompany") String numRankCompany,
+   /* String addItem( @RequestParam("UpLoadFile") MultipartFile multipartFile ,
+                   @RequestParam("rankCompany") String numRankCompany,
                    @RequestParam("numRankBlock") String numRankBlock,
                    @RequestParam("numRankStore") String numRankStore,
                    @RequestParam("Group") String Group,
@@ -91,10 +77,9 @@ public class HomeController {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, numRankCompany, numRankBlock, numRankStore,
                 Group, itemNumber, itemName, numSalesPoint);
-        taskItems.add(item);
+        taskItems.add(item);*/
 
-        return "redirect:/list";
-    }
+
 /*
     @RequestMapping("/list")
     String ListItems(){
